1f38871567fdf93ee2ad141b0cac8215dcaecd3d
roney-midasvip <roneyrick@hotmail.com>
2026-06-11 14:53:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return """
+    <h1>TESTE MIDASVIP V3</h1>
+    <p>Se você está vendo esta mensagem, o deploy novo funcionou.</p>
+    """
 
 if __name__ == "__main__":
     app.run()
