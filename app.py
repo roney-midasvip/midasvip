9540e60523d3932488bb468c074638b97168b721
roney-midasvip <roneyrick@hotmail.com>
2026-06-11 06:42:31 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -11,50 +11,21 @@ def get_db():
     conn.row_factory = sqlite3.Row
     return conn
 
-def gerar_link_final(titulo, categoria):
+def gerar_link_afiliado(titulo, categoria):
+    termo = titulo.replace(" ", "+")
     cat = (categoria or "").lower()
-    tit = (titulo or "").lower()
-    
-    # Se for algo de beleza, usa Awin
-    if any(termo in cat or termo in tit for termo in ['beleza', 'perfume', 'cosmetico']):
-        termo_busca = titulo.replace(" ", "+")
-        return f"https://www.awin1.com/cread.php?awinaffid={AWIN_ID}&p=https%3A%2F%2Fwww.belezanaweb.com.br%2Fbusca%2F%3Fq%3D{termo_busca}"
-    
-    # Se não, usa Amazon forçando categoria de moda
-    else:
-        termo_busca = titulo.replace(" ", "+")
-        return f"https://www.amazon.com.br/s?k={termo_busca}&tag={AMAZON_TAG}&i=fashion"
+    if any(t in cat for t in ['beleza', 'perfume', 'cosmetico']):
+        return f"https://www.awin1.com/cread.php?awinaffid={AWIN_ID}&p=https%3A%2F%2Fwww.belezanaweb.com.br%2Fbusca%2F%3Fq%3D{termo}"
+    return f"https://www.amazon.com.br/s?k={termo}&tag={AMAZON_TAG}&i=fashion"
 
 @app.route('/')
 def home():
     conn = get_db()
-    noticias_raw = conn.execute('SELECT * FROM noticias ORDER BY id DESC').fetchall()
-    conn.close()
-    
     noticias = []
-    for n in noticias_raw:
-        item = dict(n)
-        # O link já chega pronto para o HTML
-        item['link_final'] = gerar_link_final(item['titulo'], item.get('categoria', ''))
-        noticias.append(item)
-        
-    return render_template('index.html', noticias=noticias)
-
-@app.route('/buscar')
-def buscar():
-    termo = request.args.get('q', '')
-    conn = get_db()
-    noticias_raw = conn.execute('SELECT * FROM noticias WHERE titulo LIKE ? OR categoria LIKE ? ORDER BY id DESC', 
-                                ('%' + termo + '%', '%' + termo + '%')).fetchall()
+    for row in conn.execute('SELECT * FROM noticias ORDER BY id DESC').fetchall():
+        n = dict(row)
+        n['link_afiliado'] = gerar_link_afiliado(n['titulo'], n.get('categoria', ''))
+        noticias.append(n)
     conn.close()
-    
-    noticias = []
-    for n in noticias_raw:
-        item = dict(n)
-        item['link_final'] = gerar_link_final(item['titulo'], item.get('categoria', ''))
-        noticias.append(item)
-        
     return render_template('index.html', noticias=noticias)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# (A rota /buscar segue a mesma lógica, aplicando gerar_link_afiliado)
