ca53f82b4a80268007cf4bbcbcc802317a0f2d02
roney-midasvip <roneyrick@hotmail.com>
2026-06-11 14:55:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>TESTE MIDASVIP V3</h1>
-    <p>Se você está vendo esta mensagem, o deploy novo funcionou.</p>
-    """
+    return render_template("home.html")
+
+@app.route("/noticias")
+def noticias():
+    return render_template("noticias.html")
+
+@app.route("/bilionarios")
+def bilionarios():
+    return render_template("bilionarios.html")
+
+@app.route("/celebridades")
+def celebridades():
+    return render_template("celebridades.html")
+
+@app.route("/luxo")
+def luxo():
+    return render_template("luxo.html")
 
 if __name__ == "__main__":
     app.run()
