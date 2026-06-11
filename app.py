abfeb61d5059d27dd7470e589cf344dcbb28d6e6
roney-midasvip <roneyrick@hotmail.com>
2026-06-11 15:43:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,49 @@
 from flask import Flask, render_template
+import feedparser
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/noticias")
-def noticias():
-    return render_template("noticias.html")
 
 @app.route("/bilionarios")
 def bilionarios():
     return render_template("bilionarios.html")
 
+
 @app.route("/celebridades")
 def celebridades():
     return render_template("celebridades.html")
+
 
 @app.route("/luxo")
 def luxo():
     return render_template("luxo.html")
 
+
+@app.route("/noticias")
+def noticias():
+
+    feed = feedparser.parse(
+        "https://rss.cnn.com/rss/edition.rss"
+    )
+
+    noticias = []
+
+    for item in feed.entries[:20]:
+        noticias.append({
+            "titulo": item.title,
+            "link": item.link
+        })
+
+    return render_template(
+        "noticias.html",
+        noticias=noticias
+    )
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
