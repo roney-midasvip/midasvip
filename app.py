115b158f303870671599a4a18fc2c3667ea14cec
roney-midasvip <roneyrick@hotmail.com>
2026-06-11 16:29:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,7 @@
-from flask import Flask, render_template
-import feedparser
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
-
-@app.route("/bilionarios")
-def bilionarios():
-    return render_template("bilionarios.html")
-
-@app.route("/celebridades")
-def celebridades():
-    return render_template("celebridades.html")
-
-@app.route("/luxo")
-def luxo():
-    return render_template("luxo.html")
-
-@app.route("/noticias")
-def noticias():
-
-    feed = feedparser.parse(
-        "https://feeds.bbci.co.uk/news/world/rss.xml"
-    )
-
-    lista_noticias = []
-
-    for item in feed.entries[:20]:
-        lista_noticias.append({
-            "titulo": item.title,
-            "link": item.link
-        })
-
-    return render_template(
-        "noticias.html",
-        noticias=lista_noticias
-    )
-
-if __name__ == "__main__":
-    app.run()
+    return "<h1>MIDASVIP TESTE 999</h1>"
