ab9c5aaf9d48228878e8eb92f827f110af0e7742
roney-midasvip <roneyrick@hotmail.com>
2026-06-12 17:30:57 -0300

    Create produtos.py

---
+++ produtos.py
@@ -0,0 +1,36 @@
+produtos_por_categoria = {
+
+    "luxo": [
+        {
+            "nome": "Rolex Submariner",
+            "descricao": "O relógio mais icônico do luxo mundial",
+            "link": "https://seu-link-afiliado.com"
+        },
+        {
+            "nome": "Ferrari Experience",
+            "descricao": "Experiência exclusiva com supercarros",
+            "link": "https://seu-link-afiliado.com"
+        }
+    ],
+
+    "beleza": [
+        {
+            "nome": "Dior Sauvage",
+            "descricao": "Perfume masculino mais vendido do mundo",
+            "link": "https://seu-link-afiliado.com"
+        },
+        {
+            "nome": "Chanel No.5",
+            "descricao": "Perfume feminino clássico de luxo",
+            "link": "https://seu-link-afiliado.com"
+        }
+    ],
+
+    "moda-masculina": [
+        {
+            "nome": "Camisa Hugo Boss",
+            "descricao": "Elegância e estilo premium masculino",
+            "link": "https://seu-link-afiliado.com"
+        }
+    ]
+}
