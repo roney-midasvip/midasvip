b01de878e590f29e9cfe39a19ef8b39502f417e6
roney-midasvip <roneyrick@hotmail.com>
2026-06-12 05:30:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def buscar_noticias():
         "&language=en"
         "&sortBy=publishedAt"
         "&pageSize=12"
-        f"&apiKey={NEWS_API_KEY}"
+        f"&apiKey={7a1c6708658f493bb44176f431606bc3}"
     )
 
     try:
