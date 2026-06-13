256dee67384e9dd3cd356f0d5a445674bd2f7ab6
roney-midasvip <roneyrick@hotmail.com>
2026-06-13 19:54:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,24 +11,6 @@ API_KEY = "lmd_dev_F8K39DzOwG_Z1pGoeCk7rg1FFouHbuEdXFldOGfsIWc"
 SOURCE_ID = "fc89b7ba-30c3-4ff4-ad37-5ebfea125368"
 
 def atualizar_produtos_automaticamente():
-    """Busca produtos da API caso o arquivo ainda não exista."""
-    if os.path.exists('produtos_data.json'):
-        return 
-    
-    categorias = ["perfumes", "beleza", "moda-feminina", "relogios", "bolsas", "moda-masculina", "tecnologia", "viagens"]
-    base_produtos = {}
-
-    for cat in categorias:
-        url = f"https://api.lomadee.com/v3/{API_KEY}/product/_preferred?sourceId={SOURCE_ID}&keyword={cat}"
-        try:
-            r = requests.get(url)
-            if r.status_code == 200:
-                base_produtos[cat] = [{"nome": p['productName'], "descricao": p.get('shortDescription', ''), "link": p['link']} 
-                                      for p in r.json().get('products', [])[:5]]
-        except: pass
-    
-    with open('produtos_data.json', 'w', encoding='utf-8') as f:
-        json.dump(base_produtos, f, ensure_ascii=False, indent=4)
 
 def carregar_produtos():
     atualizar_produtos_automaticamente()
